8d213d9bdc2a50f026780c3c257e7fe515c2364a
gospherecompare <gospherecompare@gmail.com>
2026-01-22 15:19:56 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -44,7 +44,7 @@ const Login = ({ onLogin }) => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("http://apishpere.duckdns.org/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
