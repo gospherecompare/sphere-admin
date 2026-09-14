fe24064f02ddde893ec79252f69f55e0c2388294
Srinivaspandrala <rschintu3g@gmail.com>
2026-09-14 20:51:53 +0530

    update ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1159,7 +1159,7 @@ const Login = ({ onLogin }) => {
                 }));
                 setError("");
               }}
-              placeholder="admin@hooks.com"
+              placeholder="admin@mobilesx.com"
               autoComplete="email"
               required
               disabled={loading}
@@ -1470,8 +1470,8 @@ const Login = ({ onLogin }) => {
 
               <p className="mt-2 px-2 text-center text-[13px] text-white/48 sm:mt-3 sm:text-[14px]">
                 &copy; 2024{" "}
-                <span className="font-semibold text-[#3458ff]">Hooks</span>. All
-                rights reserved.
+                <span className="font-semibold text-[#3458ff]">MobilesX</span>.
+                All rights reserved.
               </p>
             </div>
           </section>
